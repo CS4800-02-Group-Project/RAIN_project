c02d8f2beb5a86f2f699c32e31b2f6c012c0ac3b
Kenneth Wang <kenneth_tl@outlook.com>
2025-03-16 22:14:36 -0700

    Json Updated

--- src-backend/main.py
+++ src-backend/main.py
@@ -14,42 +14,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
-
-
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
-
-def run():
-    """
-    Run the crew.
-    """
-    inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
-    }
-    
-    try:
-        Backend().crew().kickoff(inputs=inputs)
-    except Exception as e:
-        raise Exception(f"An error occurred while running the crew: {e}")
-
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
-    }
-    try:
-        Backend().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while testing the crew: {e}")
-
 def research(InputTopic):
     """
     Run the crew.
